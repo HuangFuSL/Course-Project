460ae02d391fd1742c51d3b09b469ba39b9c825a
HuangFuSL <huangfusl@outlook.com>
2021-07-07 22:42:59 +0800

    Removed unused exceptions

--- main/db/error.py
+++
@@ -1,7 +0,0 @@
-class NotInitiatedError(BaseException):
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(self, *args, **kwargs)
-
-    def __str__(self):
-        return "Engine not initiated"
